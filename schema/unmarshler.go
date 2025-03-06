a9bd8ffaa750e38bf22e246379597dd3c798f041
Ankit Pokhrel <oss@ankit.pl>
2025-03-06 10:07:49 +0100

    Fix MoneyV2 unmarshler

--- schema/unmarshler.go
+++ schema/unmarshler.go
@@ -6,17 +6,22 @@ import (
 )
 
 func (m *MoneyV2) UnmarshalJSON(data []byte) error {
-	var raw map[string]string
+	var raw map[string]any
 	if err := json.Unmarshal(data, &raw); err != nil {
 		return err
 	}
+	m.CurrencyCode = CurrencyCode(raw["currencyCode"].(string))
 
-	if amountStr, ok := raw["amount"]; ok {
-		amount, err := strconv.ParseFloat(amountStr, 64)
-		if err != nil {
-			return err
+	if val, isFloat := raw["amount"].(float64); isFloat {
+		m.Amount = val
+	} else {
+		if val, ok := raw["amount"]; ok {
+			amount, err := strconv.ParseFloat(val.(string), 64)
+			if err != nil {
+				return err
+			}
+			m.Amount = amount
 		}
-		m.Amount = amount
 	}
 	return nil
 }
